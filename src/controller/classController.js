4d702cfaa634f1cae5b495fc20e4541c1a9a0625
hrishita31 <hrishita.3134@gmail.com>
2025-01-29 15:45:56 +0530

    updated the new class student registration api

--- src/controller/classController.js
+++ src/controller/classController.js
@@ -2,30 +2,22 @@ import { addStudent } from '../service/classService.js';
 import '../model/classModel.js';
 import '../message/messages.js';
 import { MISSING_PARAMETER, INVALID_PHONE_NUMBER } from '../message/messages.js';
-import { successResponse } from '../response/response.js';
-
-const classOptions = async(req, res) => {
-    try{
-        const options = {
-            classTime : ['4:00-5:00', '5:00-6:00', '6:00-7:00'],
-            branch : ['Bopal', 'Shyamal'],
-        }
-       
-    }catch(error){
-
-    }
-}
+import { successResponse, errorResponse } from '../response/response.js';
+import validator from 'validator';
 
 const newClassStudent = async(req, res) => {
     try{
-        const {name, phoneNumber, classTime, branch} = req.body;
+        const {name, phoneNumber,classTime,branch} = req.body;
+        
 
         if(!name || !phoneNumber || !classTime || !branch){
             return errorResponse(res, "", 400, MISSING_PARAMETER);
         }
-        if(phoneNumber.length!=10){
+        const isValidPhoneNumber = validator.isLength(phoneNumber, {min:10, max:10})
+        if(!isValidPhoneNumber){
             return errorResponse(res, "", 400, INVALID_PHONE_NUMBER);
         }
+
         const classes = await addStudent({name, phoneNumber, classTime, branch});
         return successResponse(res, classes, 200);
     }catch(error){
@@ -33,4 +25,4 @@ const newClassStudent = async(req, res) => {
     }
 }
 
-export {classOptions, newClassStudent};
+export {newClassStudent};
